aeca481c1bcf79aa06bae70393f264b4879420e1
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-11 01:22:58 -0300

    feat: Connect db and userCreated improvements

--- src/main/java/dev/burndev/todolist/user/UserController.java
+++ src/main/java/dev/burndev/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package dev.burndev.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+  @Autowired
+  private IUserRepository userRepository;
   /**
    * String ( texto )
    * Integer (int) números inteiros
@@ -24,7 +31,17 @@ public class UserController {
    * void sem retorno
    */
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel);
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if(user != null) {
+      // Retorna:
+      // 1 - Mensagem de erro
+      // 2 - HTTP status code
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe.");
+    }
+
+    var userCreated = this.userRepository.save(userModel);
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 }
