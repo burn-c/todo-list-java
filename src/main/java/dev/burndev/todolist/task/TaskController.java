f40dca4030f6a59909250d1083ca4ce67f53d0d0
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-12 23:12:17 -0300

    feat(TaskController): adds date validations on POST and list tasks by user route

--- src/main/java/dev/burndev/todolist/task/TaskController.java
+++ src/main/java/dev/burndev/todolist/task/TaskController.java
@@ -1,8 +1,13 @@
 package dev.burndev.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +22,33 @@ public class TaskController {
   private ITaskRepository taskRepository;
 
   @PostMapping("/")
-  public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+  public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
     System.out.println("Chegou controller");
     var idUser = request.getAttribute("idUser");
     taskModel.setIdUser((UUID) idUser);
-    
+
+    var currentDate = LocalDateTime.now();
+    if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt()) ) {
+      return ResponseEntity
+      .status(HttpStatus.BAD_REQUEST)
+      .body("A data de início e data de término deve ser maior do que a data atual.");
+    }
+
+    if (taskModel.getStartAt().isAfter(taskModel.getEndAt()) ) {
+      return ResponseEntity
+      .status(HttpStatus.BAD_REQUEST)
+      .body("A data de início deve ser menor do que a data de término.");
+    }
+
     var task = this.taskRepository.save(taskModel);
-    return task;
+    return ResponseEntity.status(HttpStatus.OK).body(task);
+  }
+
+  @GetMapping("/")
+  public List<TaskModel> list(HttpServletRequest request) {
+    var idUser = request.getAttribute("idUser");
+
+    var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+    return tasks;
   }
 }
