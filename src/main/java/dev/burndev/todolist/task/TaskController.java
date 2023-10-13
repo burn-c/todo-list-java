b2173b1ba61e7b3f060374f634cd529161329084
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-13 10:51:27 -0300

    feat(TaskController): adds validations to user cannot update tasks from other users

--- src/main/java/dev/burndev/todolist/task/TaskController.java
+++ src/main/java/dev/burndev/todolist/task/TaskController.java
@@ -56,11 +56,23 @@ public class TaskController {
   }
 
   @PutMapping("/{id}")
-  public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+  public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
     var task = this.taskRepository.findById(id).orElse(null);
+
+    var idUser = request.getAttribute("idUser");
+
+    if (task == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada!");
+    }
+
+    if (!task.getIdUser().equals(idUser)) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não tem permissão para alterar essa tarefa!");
+    }
 
     Utils.copyNonNullProperty(taskModel, task);
 
-    return this.taskRepository.save(task);
+    var taskUpdated = this.taskRepository.save(task);
+
+    return ResponseEntity.status(HttpStatus.OK).body(taskUpdated);
   }
 }
